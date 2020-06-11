edf487bc58072b1ad63a26d42606412c457837bb
kibohopu39 <changewiththetime@gmail.com>
2020-06-11 17:55:15 +0800

    完成猜數字基本功能

--- app/src/main/java/com/example/guessnumber/MainActivity.java
+++ app/src/main/java/com/example/guessnumber/MainActivity.java
@@ -4,18 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
     private Button n1,n2,n3,n4,n5,n6,n7,n8,n9,n0,del,clr, guess,replay;
-    private input_handler IH;
     private TextView log,input1,input2,input3,input4;
-
+    private String answer;
+    private int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,86 +42,201 @@ public class MainActivity extends AppCompatActivity {
         input3=findViewById(R.id.input3);
         input4=findViewById(R.id.input4);
         log=findViewById(R.id.log);
-
-
-        IH=new input_handler();
-        n1.setOnClickListener(IH);
-        n2.setOnClickListener(IH);
-        n3.setOnClickListener(IH);
-        n4.setOnClickListener(IH);
-        n5.setOnClickListener(IH);
-        n6.setOnClickListener(IH);
-        n7.setOnClickListener(IH);
-        n8.setOnClickListener(IH);
-        n9.setOnClickListener(IH);
-        n0.setOnClickListener(IH);
-
-
-
-
-        //initNewGame();
+        initGame();
     }
 
-    private class input_handler implements View.OnClickListener {
-
-        @Override
-        public void onClick(View v) {
-
-
-
-
-  
-
-
-
-          }
-    }
-    private String answer;
-    public void initNewGame(){//起新局
-        answer=createAnswer();
-        input1.setText("");
-        log.setText("");
-    }
-
-    private LinkedList<Integer> number;
-    private int challange;
-    private String createAnswer(){//建立答案
-        for(int i=0;i<10;i++) {//產出10個數字放入list
-            number.add(i);
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
         }
-        Collections.shuffle(number);//打亂順序
-        StringBuffer sb=new StringBuffer();//建立最後要丟出的答案
-        for(int j=0;j<challange;j++){
-            sb.append(number.get(j));//取?個數字出來放到sb
+    }
+
+
+
+
+    //時間戳記來增加金錢,即今天開啟本遊戲就+多少錢
+    //本難度沒有提示,因此不會花到金錢
+    //答對可以獲得獎勵
+    //達到猜題次數上限卻沒有猜到,就是挑戰失敗
+    //可以調整難度
+    //增加難度就會增加提示按鈕,該按鈕可以顯示出數字但最多顯示一次
+
+    private void initGame(){
+        //遊戲初始化
+
+        answer=createAnswer();//創建答案
+
+        log.setText("");//把猜的紀錄刪除
+        count=0;//把猜的次數規零
+        clear(null);//把最後一次的輸入清空
+        MainApp.guesstimes=8;//把次數設定好
+
+
+    }
+
+
+
+    //首先要解決我輸入到哪一格了,才能去判斷我按的數字要放到哪個input中
+    public int whichinput(){
+        int index=0;
+        //依序判斷每個input裡有沒有值,分別重左到右
+        int input[]={input1.length(),input2.length(),input3.length(),input4.length()};
+        for (int hasNumber:input
+             ) {
+            if (hasNumber==0){
+                index++;
+            }
+        }
+        //index==1--->input1沒數字(第一圈就停)
+        //index==2--->input1,2有數字(第二圈停)
+        //index==3--->input1,2,3沒數字(第三圈停)
+        //index==4--->input1,2,3,4沒數字(第四圈停)
+        //index==0--->都有數字(第五圈停,都沒進入判斷所以是0)
+        return index;//
+    }
+
+    //輸入數字
+    public void input(View v){
+        int i=whichinput();//要在哪一格設定數字
+        int n=0;
+        //確定按了哪個按鈕
+        int btn[]={n0.getId(),n1.getId(),n2.getId(),n3.getId(),n4.getId(),n5.getId(),n6.getId(),n7.getId(),n8.getId(),n9.getId()};
+        for (n=0;n<10;n++){
+            if (v.getId()==btn[n]){
+                break;
+            }
+        }
+        setInputnumber(n,i);
+    }
+    // 把數字設置到Input中
+    public void setInputnumber(int whatNumber,int whichInputText){
+        //先判斷是不是有重複輸入,即按的數字必須與input中的不一樣才可以印出
+        CharSequence num=""+whatNumber;
+        String InputNumber[]={"0","1","2","3","4","5","6","7","8","9"};
+        if (!input1.getText().equals(num) & !input2.getText().equals(num) & !input3.getText().equals(num)){
+            switch (whichInputText){
+                case 4:
+                    input1.setText(InputNumber[whatNumber]);
+                    break;
+                case 3:
+                    input2.setText(InputNumber[whatNumber]);
+                    break;
+                case 2:
+                    input3.setText(InputNumber[whatNumber]);
+                    break;
+                case 1:
+                    input4.setText(InputNumber[whatNumber]);
+                    break;
+                case 0:
+                    break;
+            }
+        }
+    }
+
+    //做出答案
+    public String createAnswer(){
+        ArrayList pool=new ArrayList<>();
+        for (int n=0;n<10;n++){
+            pool.add(n);
+        }
+        Collections.shuffle(pool);//四個數字要不一樣
+        StringBuffer sb=new StringBuffer();
+        for (int i=0;i<4;i++){
+            sb.append(pool.get(i));
         }
         return sb.toString();
     }
-    private String checkguess(String guess){//鑑定猜測
-        int A=0,B=0;
-        for (int i=0; i<answer.length(); i++) {
-            if (answer.charAt(i) == guess.charAt(i)) {
+
+
+    public void clear(View view) {//清空
+        input1.setText("");
+        input2.setText("");
+        input3.setText("");
+        input4.setText("");
+    }
+
+    public void guess(View view) {//確定送出答案
+        //檢查是否都輸入了
+        if (whichinput()>0){
+            return;
+        }
+        //把input裡的四個數字組合起來傳給check對答案
+        String strInput=(String) input1.getText()+input2.getText()+input3.getText()+input4.getText();
+        String result=check(strInput);
+        CharSequence temp=log.getText();//取得上一次輸入的
+        log.setText(strInput + " => " + result + "\n" + temp);//上一次輸入的紀錄放下面
+        clear(null);//清空上一次輸入的
+
+        //如果答對,跳出獲勝自定義的Snackbar
+        if (result.equals("4A0B")){
+
+        }
+
+
+    }
+    // 確定送出答案後要檢查
+    private String check(String guess){
+        int A,B;A=B=0;
+        for(int i=0;i<answer.length();i++){
+            if (answer.charAt(i)==guess.charAt(i)){
                 A++;
-            }else if(answer.indexOf(guess.charAt(i)) != -1) {
+            }else if (answer.indexOf(guess.charAt(i))>0){
                 B++;
             }
         }
         return A+"A"+B+"B";
     }
 
-    public void doreplay(View view) {//重啟新局
+    public void doreplay(View view) {//重玩
+        initGame();
     }
 
-    public void guess(View view) {//送出答案
-        String strInput=input1.getText().toString();
-        String result=checkguess(strInput);
-        log.append(strInput+"---------"+result+"\n");
-        input1.setText("");
+    public void delete(View view) {//由右到左刪除一位數字
+        //首先要確定目前要刪誰
+        int i= whichinput();
+        switch (i){
+            case 4://都沒數字
+                break;
+            case 3:
+                input1.setText("");
+                break;
+            case 2:
+                input2.setText("");
+                break;
+            case 1:
+                input3.setText("");
+                break;
+            case 0://都有數字
+                input4.setText("");
+                break;
+        }
     }
-
-    public void clear(View view) {//清空
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
     }
-
-    public void input(View view) {
-
+    long lastExitTime=0;
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getKeyCode() == KeyEvent.KEYCODE_BACK) {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && event.getRepeatCount() == 0) {
+                // 判断2次点击事件时间
+                if ((System.currentTimeMillis() - lastExitTime) > 2000) {
+                    Toast.makeText(MainActivity.this, "再按一次退出遊戲",Toast.LENGTH_SHORT).show();
+                    lastExitTime = System.currentTimeMillis();
+                } else {
+                    finish();
+                }
+            }
+            return true;
+        }
+        return super.dispatchKeyEvent(event);
     }
 }
---
+++ app/src/main/java/com/example/guessnumber/MainApp.java
@@ -0,0 +1,12 @@
+package com.example.guessnumber;
+
+import android.app.Application;
+
+public class MainApp extends Application {
+    public static int guesstimes;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
