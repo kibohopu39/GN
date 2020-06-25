ccff70be7684b6ee618750229d788c93f86963fc
kibohopu39 <changewiththetime@gmail.com>
2020-06-25 22:39:45 +0800

    修正ｂｕｇ

--- app/src/main/java/com/example/guessnumber/MainApp.java
+++ app/src/main/java/com/example/guessnumber/MainApp.java
@@ -4,7 +4,7 @@ import android.app.Application;
 
 public class MainApp extends Application {
     public static int guesstimes;
-    public static int guessplayingStage=2;
+    public static int guessplayingStage=1;
     @Override
     public void onCreate() {
         super.onCreate();
